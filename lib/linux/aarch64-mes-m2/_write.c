c0712bcdd7a93ff6d28328f875bed143a2bcd54e
Alexandre Gomes Gaigalas <alganet@gmail.com>
2026-05-05 16:28:01 -0300

    aarch64: M2-Planet bootstrap path (kaem + lib + include)
    Adds the file set needed for the in-image M2-Planet path to build
    mes on aarch64-Linux. Pipeline: M2-Planet -> blood-elf -> M1 ->
    hex2 -> bin/mes, mirroring the riscv64 backend's structure.
    
    New files:
    
      kaem.aarch64
        Entry script (cc_cpu/mes_cpu/stage0_cpu = aarch64,
        blood_elf_flag=--64).
    
      lib/m2/aarch64/aarch64_defs.M1
        Verbatim copy of M2libc/aarch64/aarch64_defs.M1 with three
        mes-specific extensions appended:
          SET_X8_FROM_X0  (mov x8, x0; for syscall.c routing the
                           runtime sys_call number into x8)
          SUB_X0_48
          SUB_X0_56       (extends the existing SUB_X0_8/16/24/32/40
                           series so 5- and 6-arg syscall wrappers
                           can address the higher frame-pointer
                           offsets)
    
      lib/m2/aarch64/ELF-aarch64.hex2
        Verbatim copy of M2libc/aarch64/ELF-aarch64.hex2; e_machine
        = 0xB7 (AArch64), position-independent in --base-address.
    
      lib/aarch64-mes/aarch64.M1
        Empty stub. M2-Planet's aarch64 backend emits macro mnemonics
        (SET_X0_FROM_BP-style) directly via aarch64_defs.M1; no
        arch-specific mes-mode mnemonic table is needed for the
        M2-Planet path. Kept present because scripts/mes.kaem
        references lib/${MES_CPU}-mes/${MES_CPU}.M1 unconditionally.
    
      lib/linux/aarch64-mes-m2/{crt1.c, _exit.c, _write.c, syscall.c}
        Mes-on-Linux libc-mini surfaces. crt1.c is the arch-independent
        declaration block. _exit.c, _write.c, syscall.c contain inline
        asm using the SET_X0_FROM_BP / SUB_X0_N / DEREF_X0 idiom from
        M2libc/aarch64/linux/unistd.c.
    
      lib/linux/aarch64-mes-m2/crt1.M1
        Entry trampoline. Captures sp -> x18 (INIT_SP), sets BP
        (x17) = original sp, pushes argc/argv/envp so [BP-8],
        [BP-16], [BP-24] match M2-Planet's calling convention,
        calls FUNCTION___init_io and FUNCTION_main, then exits via
        SYS_EXIT. Mirrors M2libc/aarch64/libc-core.M1 with the
        addition of the __init_io call mes wants.
    
      include/linux/aarch64/syscall.h
        asm-generic syscall numbers (read=63, write=64, exit=93, etc.).
        Same set as include/linux/riscv64/syscall.h since both arches
        use the asm-generic table.
    
      include/linux/aarch64/kernel-stat.h
        asm-generic struct stat for 64-bit, identical layout to
        riscv64's.
    
      include/linux/aarch64/signal.h
        musl-derived ucontext / mcontext / sigcontext for aarch64.
        Not on the m2-build path; included for future-proofing /
        upstream-PR completeness.

---
+++ lib/linux/aarch64-mes-m2/_write.c
@@ -0,0 +1,45 @@
+/* -*-comment-start: "//";comment-end:""-*-
+ * GNU Mes --- Maxwell Equations of Software
+ * Copyright © 2018,2020,2023 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ * Copyright © 2021 W. J. van der Laan <laanwj@protonmail.com>
+ * Copyright © 2023 Andrius Štikonas <andrius@stikonas.eu>
+ * Copyright © 2026 Alexandre Gomes Gaigalas (aarch64 port)
+ *
+ * This file is part of GNU Mes.
+ *
+ * GNU Mes is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * GNU Mes is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Mes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "mes/lib-mini.h"
+
+void
+_write (int filedes, void const *buffer, size_t size)
+{
+  /* aarch64 Linux ABI: arg1=x0, arg2=x1, arg3=x2, syscall_num=x8.
+   * Args are stored at fp-8, fp-16, fp-24 by M2-Planet's save-frame.
+   * Load LATER args first so x0 doesn't get clobbered before its
+   * value is captured.
+   *
+   *   arg3 (size)    fp-24 -> x2
+   *   arg2 (buffer)  fp-16 -> x1
+   *   arg1 (filedes) fp-8  -> x0
+   */
+  asm ("SET_X0_FROM_BP" "SUB_X0_24" "DEREF_X0"
+       "SET_X2_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_16" "DEREF_X0"
+       "SET_X1_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_8" "DEREF_X0"
+       "SET_X8_TO_SYS_WRITE"
+       "SYSCALL");
+}
---
+++ lib/linux/aarch64-mes-m2/syscall.c
@@ -0,0 +1,212 @@
+/* -*-comment-start: "//";comment-end:""-*-
+ * GNU Mes --- Maxwell Equations of Software
+ * Copyright © 2016,2017,2018,2020,2023 Jan (janneke) Nieuwenhuizen <janneke@gnu.org>
+ * Copyright © 2021 W. J. van der Laan <laanwj@protonmail.com>
+ * Copyright © 2023 Andrius Štikonas <andrius@stikonas.eu>
+ * Copyright © 2026 Alexandre Gomes Gaigalas (aarch64 port)
+ *
+ * This file is part of GNU Mes.
+ *
+ * GNU Mes is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or (at
+ * your option) any later version.
+ *
+ * GNU Mes is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with GNU Mes.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <errno.h>
+#include <linux/aarch64/syscall.h>
+
+/* aarch64 Linux ABI: x0..x5 = args, x8 = syscall number, SVC #0 = trap.
+ * Same convention as riscv64 except for register names and trap mnemonic.
+ * M2-Planet's save-frame puts arg N at fp-(N*8):
+ *   sys_call -> fp-8, one -> fp-16, two -> fp-24, three -> fp-32,
+ *   four     -> fp-40, five -> fp-48.
+ *
+ * Loading order matters: x0 is the scratch register for the
+ * SET_X0_FROM_BP / SUB_X0_N / DEREF_X0 idiom. Once a value lands in
+ * its target register (x1..x4 or x8), it survives subsequent loads.
+ * So we load:
+ *   1. sys_call -> x0 -> SET_X8_FROM_X0           (x8 = syscall #)
+ *   2. higher-index args -> x0 -> SET_Xn_FROM_X0  (xN = arg N)
+ *   3. arg1 (one) -> x0                           (x0 = arg 1)
+ *   4. SVC
+ *   5. SET_X16_FROM_X0                            (mes ABI epilogue idiom,
+ *                                                  mirrors riscv64's
+ *                                                  `mv t0, a0` -- return
+ *                                                  value relayed via x16)
+ *
+ * SET_X8_FROM_X0 is a mes-specific addition to aarch64_defs.M1
+ * (M2libc only emits SET_X8_TO_SYS_* constants, never X8 from a
+ * runtime register).
+ */
+
+long
+__sys_call (long sys_call)
+{
+  asm ("SET_X0_FROM_BP" "SUB_X0_8" "DEREF_X0"
+       "SET_X8_FROM_X0"
+       "SYSCALL"
+       "SET_X16_FROM_X0");
+}
+
+long
+__sys_call1 (long sys_call, long one)
+{
+  asm ("SET_X0_FROM_BP" "SUB_X0_8" "DEREF_X0"
+       "SET_X8_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_16" "DEREF_X0"
+       "SYSCALL"
+       "SET_X16_FROM_X0");
+}
+
+long
+__sys_call2 (long sys_call, long one, long two)
+{
+  asm ("SET_X0_FROM_BP" "SUB_X0_8" "DEREF_X0"
+       "SET_X8_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_24" "DEREF_X0"
+       "SET_X1_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_16" "DEREF_X0"
+       "SYSCALL"
+       "SET_X16_FROM_X0");
+}
+
+long
+__sys_call3 (long sys_call, long one, long two, long three)
+{
+  asm ("SET_X0_FROM_BP" "SUB_X0_8" "DEREF_X0"
+       "SET_X8_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_32" "DEREF_X0"
+       "SET_X2_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_24" "DEREF_X0"
+       "SET_X1_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_16" "DEREF_X0"
+       "SYSCALL"
+       "SET_X16_FROM_X0");
+}
+
+long
+__sys_call4 (long sys_call, long one, long two, long three, long four)
+{
+  asm ("SET_X0_FROM_BP" "SUB_X0_8" "DEREF_X0"
+       "SET_X8_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_40" "DEREF_X0"
+       "SET_X3_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_32" "DEREF_X0"
+       "SET_X2_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_24" "DEREF_X0"
+       "SET_X1_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_16" "DEREF_X0"
+       "SYSCALL"
+       "SET_X16_FROM_X0");
+}
+
+long
+__sys_call5 (long sys_call, long one, long two, long three, long four, long five)
+{
+  asm ("SET_X0_FROM_BP" "SUB_X0_8" "DEREF_X0"
+       "SET_X8_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_48" "DEREF_X0"
+       "SET_X4_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_40" "DEREF_X0"
+       "SET_X3_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_32" "DEREF_X0"
+       "SET_X2_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_24" "DEREF_X0"
+       "SET_X1_FROM_X0"
+       "SET_X0_FROM_BP" "SUB_X0_16" "DEREF_X0"
+       "SYSCALL"
+       "SET_X16_FROM_X0");
+}
+
+long
+_sys_call (long sys_call)
+{
+  long r = __sys_call (sys_call);
+  if (r < 0)
+    {
+      errno = -r;
+      r = -1;
+    }
+  else
+    errno = 0;
+  return r;
+}
+
+long
+_sys_call1 (long sys_call, long one)
+{
+  long r = __sys_call1 (sys_call, one);
+  if (r < 0)
+    {
+      errno = -r;
+      r = -1;
+    }
+  else
+    errno = 0;
+  return r;
+}
+
+long
+_sys_call2 (long sys_call, long one, long two)
+{
+  long r = __sys_call2 (sys_call, one, two);
+  if (r < 0)
+    {
+      errno = -r;
+      r = -1;
+    }
+  else
+    errno = 0;
+  return r;
+}
+
+long
+_sys_call3 (long sys_call, long one, long two, long three)
+{
+  long r = __sys_call3 (sys_call, one, two, three);
+  if (r < 0)
+    {
+      errno = -r;
+      r = -1;
+    }
+  else
+    errno = 0;
+  return r;
+}
+
+long
+_sys_call4 (long sys_call, long one, long two, long three, long four)
+{
+  long r = __sys_call4 (sys_call, one, two, three, four);
+  if (r < 0)
+    {
+      errno = -r;
+      r = -1;
+    }
+  else
+    errno = 0;
+  return r;
+}
+
+long
+_sys_call5 (long sys_call, long one, long two, long three, long four, long five)
+{
+  long r = __sys_call5 (sys_call, one, two, three, four, five);
+  if (r < 0)
+    {
+      errno = -r;
+      r = -1;
+    }
+  else
+    errno = 0;
+  return r;
+}
